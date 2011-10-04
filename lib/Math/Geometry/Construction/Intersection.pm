35dd19ca38e95cf3f090626da515aba651a48318
Lutz Gehlen <lgehlen@cpan.org>
2011-10-05 10:36:18 +1300

    new class Intersection and new role Output
    Output contains the graphic stuff because I want Intersection to
    consume Object without getting style etc.; specific_poi is currently
    in Output, but does not really belong there; I leave it for now,
    though, because I will have to make up my mind where it should go.

---
+++ lib/Math/Geometry/Construction/Intersection.pm
@@ -0,0 +1,106 @@
+package Math::Geometry::Construction::Intersection;
+
+use 5.008008;
+
+use Moose;
+use Math::VectorReal;
+use Carp;
+
+=head1 NAME
+
+C<Math::Geometry::Construction::Intersection> - intersection of objects
+
+=head1 VERSION
+
+Version 0.002
+
+=cut
+
+our $VERSION = '0.002';
+
+
+###########################################################################
+#                                                                         #
+#                      Class Variables and Methods                        # 
+#                                                                         #
+###########################################################################
+
+our $ID_TEMPLATE = 'I%09d';
+
+sub id_template { return $ID_TEMPLATE }
+
+###########################################################################
+#                                                                         #
+#                               Accessors                                 # 
+#                                                                         #
+###########################################################################
+
+with 'Math::Geometry::Construction::Object';
+
+###########################################################################
+#                                                                         #
+#                             Retrieve Data                               #
+#                                                                         #
+###########################################################################
+
+###########################################################################
+#                                                                         #
+#                              Change Data                                # 
+#                                                                         #
+###########################################################################
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 INTERFACE
+
+=head2 Public Attributes
+
+=head2 Methods for Users
+
+=head2 Methods for Subclass Developers
+
+=head3 as_svg
+
+=head3 id_template
+
+=head1 DIAGNOSTICS
+
+=head2 Exceptions
+
+=head2 Warnings
+
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported. Please report all bugs directly to the author.
+
+
+=head1 AUTHOR
+
+Lutz Gehlen, C<< <perl at lutzgehlen.de> >>
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Lutz Gehlen.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
---
+++ lib/Math/Geometry/Construction/Output.pm
@@ -0,0 +1,108 @@
+package Math::Geometry::Construction::Output;
+use Moose::Role;
+
+use 5.008008;
+
+use Carp;
+
+=head1 NAME
+
+C<Math::Geometry::Construction::Output> - graphical output issues
+
+=head1 VERSION
+
+Version 0.002
+
+=cut
+
+our $VERSION = '0.002';
+
+
+###########################################################################
+#                                                                         #
+#                               Accessors                                 # 
+#                                                                         #
+###########################################################################
+
+has 'style'       => (isa     => 'HashRef[Str]',
+		      is      => 'rw',
+		      reader  => 'style_hash',
+		      writer  => '_style_hash',
+		      traits  => ['Hash'],
+		      default => sub { {} },
+		      handles => {style => 'accessor'});
+
+has 'hidden'      => (isa     => 'Bool',
+		      is      => 'rw',
+		      default => 0);
+
+has 'specific_poi' => (isa     => 'ArrayRef[Item]',
+		       is      => 'bare',
+		       traits  => ['Array'],
+		       default => sub { [] },
+		       handles => {specific_poi => 'elements',
+				   add_poi      => 'push'});
+
+###########################################################################
+#                                                                         #
+#                             Retrieve Data                               #
+#                                                                         #
+###########################################################################
+
+###########################################################################
+#                                                                         #
+#                              Change Data                                # 
+#                                                                         #
+###########################################################################
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=head1 INTERFACE
+
+=head2 Public Attributes
+
+=head2 Methods for Users
+
+=head2 Methods for Subclass Developers
+
+=head1 DIAGNOSTICS
+
+=head2 Exceptions
+
+=head2 Warnings
+
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported. Please report all bugs directly to the author.
+
+
+=head1 AUTHOR
+
+Lutz Gehlen, C<< <perl at lutzgehlen.de> >>
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Lutz Gehlen.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
