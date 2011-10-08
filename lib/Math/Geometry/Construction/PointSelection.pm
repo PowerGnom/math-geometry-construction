f05ab4e40350334cbd360f06b7bf5bb4ba82b38c
Lutz Gehlen <lgehlen@cpan.org>
2011-10-08 15:58:31 +1300

    bumped version to 0.003

--- lib/Math/Geometry/Construction/PointSelection.pm
+++ lib/Math/Geometry/Construction/PointSelection.pm
@@ -11,11 +11,11 @@ C<Math::Geometry::Construction::PointSelection> - how far to draw?
 
 =head1 VERSION
 
-Version 0.002
+Version 0.003
 
 =cut
 
-our $VERSION = '0.002';
+our $VERSION = '0.003';
 
 
 ###########################################################################
