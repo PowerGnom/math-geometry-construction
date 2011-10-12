efa92bd2f7f7418a2b97146a674d16972ef899a2
Lutz Gehlen <lgehlen@cpan.org>
2011-10-12 19:51:13 +1300

    DESCRIPTION

--- lib/Math/Geometry/Construction.pm
+++ lib/Math/Geometry/Construction.pm
@@ -146,8 +146,115 @@ __END__
 
   print $construction->as_svg(width => 800, height => 300)->xmlify;
 
+
 =head1 DESCRIPTION
 
+This is alpha software. The API is likely to change, input checks
+and documentation are sparse, the test suite barely exists. But
+release early, release often, so here we go.
+
+=head2 Aims
+
+This distribution serves two purposes:
+
+=over 4
+
+=item * Carrying out geometric constructions like with compass and
+straight edge. You can define points, lines through two points, and
+(in the future) circles around a given center and through a given
+point. You can let these objects intersect to gain new points to
+work with.
+
+=item * Creating illustrations for geometry. This task is similar to
+the one above, but not the same. For illustrations, the priorities
+are usually different and more powerful tools like choosing a point
+on a given object, drawing circles with fixed radius etc. are handy.
+
+=back
+
+=head2 Motivation
+
+Problems of these kinds can be solved using several pieces of
+geometry software. However, I have not found any with sufficiently
+powerful automization features. This problem has two aspects:
+
+=over 4
+
+=item * For some project, I want to create many illustrations. I
+have certain rules for the size of output images, usage of colors
+etc.. With the programs I have used so far, I have found it
+difficult to set these things in a consistent way for all of my
+illustrations without the need to set them each time I start the
+program or to change consistently later on.
+
+=item * For actual constructions, most macro languages are not
+powerful enough. For example, the intersection between two circles
+sometimes yields two points. There are situations where I want to
+choose the one which is further away from some given point or the
+one which is not the one I already had before or things like
+that. The macro languages of most geometry programs do not allow
+that. It is somehow determined internally which one is first
+intersection point and which the second, so from the user point of
+view the choice is arbitrary. Or, for example, I have come across
+the situation where I needed to double an angle iteratively until it
+becomes larger than a given angle. Impossible in most macro
+languates. With this module, you have Perl as your "macro language".
+
+=head2 Intersection Concept
+
+Intersecting two objects consists of two steps. First, you create a
+L<Math::Geometry::Construction::Derivate|Math::Geometry::Construction::Derivate>
+object that holds the intersecting partners and "knows" how to
+calculate the intersection points. Second, you create a
+L<Math::Geometry::Construction::DerivedPoint|Math::Geometry::Construction::DerivedPoint>
+from the C<Derivate>. The C<DerivedPoint> object holds information
+about which of the intersection points to use. This can be based
+distance to a given point, being the extreme point with respect to a
+given direction etc..
+
+The C<DerivedPoint> object only holds information about how to
+select the right point. Only when you actually ask for the position
+of the point it is actually calculated.
+
+The classes are called C<Derivate> and C<DerivedPoint> because this
+concept is applicable beyond the case of intersections. It could,
+for example, be used to calculate the center of a circle given by
+three points. Whenever some operation based on given objects results
+in a finite number of points, it fits into this concept.
+
+=head2 Output
+
+Each line or similar object holds a number of "points of
+interest". This are - in case of the line - the two points that
+define the line and all intersection points the line is involved
+in. At drawing time, the object determines the most extreme points
+and they define the end points of the drawn line segment. The
+C<extend> attribute allows to extend the line for a given length
+beyond these points because this often looks better. A similar
+concept will be implemented for circles and other objects.
+
+=head2 Current Status
+
+At the moment, you can define points and lines, and you can
+intersect lines. Points and lines can have labels. Essentially, the
+whole current functionality is summarized in the
+L<SYNOPSIS|SYNOPSIS>.
+
+=head2 Next Steps
+
+=over 4
+
+=item * Circles
+
+=item * Extend documentation
+
+=item * Improve performance
+
+=item * Improve automatic positioning of labels
+
+=item * Improve test suite along the way
+
+=back
 
 =head1 INTERFACE
 
