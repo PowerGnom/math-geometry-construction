e060f6d8b5cca9003bb802901c4fd70e7c83f5d1
Lutz Gehlen <lgehlen@cpan.org>
2011-10-13 17:33:31 +1300

    first try, syntax ok

--- lib/Math/Geometry/Construction/Derivate/IntersectionCircleLine.pm
+++ lib/Math/Geometry/Construction/Derivate/IntersectionCircleLine.pm
@@ -1,24 +1,23 @@
-package Math::Geometry::Construction::Derivate::IntersectionLineLine;
+package Math::Geometry::Construction::Derivate::IntersectionCircleLine;
 use base 'Math::Geometry::Construction::Derivate';
 
 use 5.008008;
 
 use Carp;
 use Math::VectorReal ':all';
-use Math::MatrixReal;
 use Math::Geometry::Construction::TemporaryPoint;
 
 =head1 NAME
 
-C<Math::Geometry::Construction::Derivate::IntersectionLineLine> - line line intersection
+C<Math::Geometry::Construction::Derivate::IntersectionCircleLine> - circle line intersection
 
 =head1 VERSION
 
-Version 0.003
+Version 0.005
 
 =cut
 
-our $VERSION = '0.003';
+our $VERSION = '0.005';
 
 
 ###########################################################################
@@ -35,42 +34,77 @@ our $VERSION = '0.003';
 
 sub points {
     my ($self) = @_;
-    my @lines  = $self->input;
+    my @input  = $self->input;
 
-    croak "Needs two lines to intersect" if(@lines != 2);
-    foreach(@lines) {
-	if(!$_->isa('Math::Geometry::Construction::Line')) {
-	    croak sprintf("Need lines for LineLine intersection, no %s",
-			  ref($_));
-	}
+    croak "Need one circle and one line to intersect" if(@input != 2);
+
+    my $circle;
+    my $line;
+    if(eval { $input[0]->isa('Math::Geometry::Construction::Circle') }
+       and
+       eval { $input[1]->isa('Math::Geometry::Construction::Line') })
+    {
+	($circle, $line) = @input;
+    }
+    elsif(eval { $input[0]->isa('Math::Geometry::Construction::Line') }
+	  and
+	  eval { $input[1]->isa('Math::Geometry::Construction::Circle') })
+    {
+	($line, $circle) = @input;
+    }
+    else { croak "Need one circle and one line to intersect"  }
+       
+    my $center_p    = $circle->center->position;
+    my $c_support_p = $circle->support->position;
+    my @l_support_p = map { $_->position } $line->support;
+
+    foreach my $_ ($center_p, $c_support_p, @l_support_p) {
+	return if(!defined($_));
     }
 
-    my @support  = map { [map { $_->position } $_->support] } @lines;
+    # line normal
+    my $l_normal   =
+	(plane(Z, O, $l_support_p[1] - $l_support_p[0]))[0]->norm;
 
-    foreach my $line (@support) {
-	foreach my $position (@$line) {
-	    return if(!defined($position));
-	}
+    # some shorter and/or more intuitive variables
+    my $a  = $l_normal->x;
+    my $b  = $l_normal->y;
+    my $c  = $l_normal . $l_support[0];
+    my $cx = $center_p->x;
+    my $cy = $center_p->y;
+    my $r  = ($c_support_p - $center_p)->length;
+
+    my @x;
+    my @y;
+    # quadratic equation
+    if(abs($a) >= abs($b)) {
+	my $denom = ($b/$a)**2 + 1;
+	my $mph   = ($b*$c/$a**2 + $cy - $b/$a*$cx) / $denom;
+	my $q     = (($cx - $c/$a)**2 + $cy**2 - $r**2) / $denom;
+
+	my $rad = $mph**2 - $q;
+	return if($rad < 0);
+
+	my $root = sqrt($rad);
+	@y = ($mph + $root, $mph - $root);
+	@x = map { ($c - $b * $_) / $a } @y;
+    }
+    else {
+	my $denom = ($a/$b)**2 + 1;
+	my $mph   = ($a*$c/$b**2 + $cx - $a/$b*$cy) / $denom;
+	my $q     = (($cy - $c/$b)**2 + $cx**2 - $r**2) / $denom;
+
+	my $rad = $mph**2 - $q;
+	return if($rad < 0);
+
+	my $root = sqrt($rad);
+	@x = ($mph + $root, $mph - $root);
+	@y = map { ($c - $a * $_) / $b } @x;
     }
 
-    my @normal   = map { $_->norm }
-	           map { (plane(Z, O, $_->[1] - $_->[0]))[0] } @support;
-    my @constant = map { $normal[$_] . $support[$_]->[0] } (0, 1);
-    my $matrix   = Math::MatrixReal->new_from_rows
-	([map { [$_->x, $_->y] } @normal]);
-
-    return if($matrix->det == 0);
-    my $inverse = $matrix->inverse;
-    return if(!$inverse);  # only possible - if at all - for num. reasons
-
-    my $position = vector($inverse->element(1, 1) * $constant[0] +
-			  $inverse->element(1, 2) * $constant[1],
-			  $inverse->element(2, 1) * $constant[0] +
-			  $inverse->element(2, 2) * $constant[1],
-			  0);
-
-    return(Math::Geometry::Construction::TemporaryPoint->new
-	   (position => $position));
+    my @positions = map { vector($x[$_], $y[$_], 0) } (0, 1);
+    my $class     = 'Math::Geometry::Construction::TemporaryPoint';
+    return(map { $class->new(position => $_) } @positions);
 }
 
 ###########################################################################
