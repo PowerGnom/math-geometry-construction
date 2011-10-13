fe5dbf0a0a9c4c6d6a2c440ab74e743fe392a70b
Lutz Gehlen <lgehlen@cpan.org>
2011-10-14 11:51:20 +1300

    seems to work

--- lib/Math/Geometry/Construction/Derivate/IntersectionCircleCircle.pm
+++ lib/Math/Geometry/Construction/Derivate/IntersectionCircleCircle.pm
@@ -35,47 +35,39 @@ our $VERSION = '0.005';
 ###########################################################################
 
 sub points {
-    my ($self) = @_;
-    my @input  = $self->input;
+    my ($self)  = @_;
+    my @circles = $self->input;
 
-    croak "Need one circle and one line to intersect" if(@input != 2);
-
-    my $circle;
-    my $line;
-    if(eval { $input[0]->isa('Math::Geometry::Construction::Circle') }
-       and
-       eval { $input[1]->isa('Math::Geometry::Construction::Line') })
-    {
-	($circle, $line) = @input;
+    croak "Need two circles to intersect" if(@circles != 2);
+    foreach(@circles) {
+	if(!$_->isa('Math::Geometry::Construction::Circle')) {
+	    croak sprintf("Need circles for CircleCircle intersection, ".
+			  "no %s", ref($_));
+	}
     }
-    elsif(eval { $input[0]->isa('Math::Geometry::Construction::Line') }
-	  and
-	  eval { $input[1]->isa('Math::Geometry::Construction::Circle') })
-    {
-	($line, $circle) = @input;
-    }
-    else { croak "Need one circle and one line to intersect"  }
-       
-    my $c_center_p  = $circle->center->position;
-    my $c_support_p = $circle->support->position;
-    my @l_support_p = map { $_->position } $line->support;
 
-    foreach my $_ ($c_center_p, $c_support_p, @l_support_p) {
+    # currently assuming that points have to be defined
+    my @center_p = map { $_->center->position  } @circles;
+    my @radii    = map { $_->radius            } @circles;
+
+    foreach(@center_p, @radii) {
 	return if(!defined($_));
     }
 
-    my $l_parallel = ($l_support_p[1] - $l_support_p[0])->norm;
-    my $l_normal   = vector($l_parallel->y, -$l_parallel->x, 0);
-    my $l_constant = $l_normal . $l_support_p[0];
-    my $c_radius   = ($c_support_p - $c_center_p)->length;
+    my $distance = ($center_p[1] - $center_p[0]);
+    my $d        = $distance->length;
+    return if($d == 0);
 
-    my $a   = $l_normal . $c_center_p - $l_constant;
-    my $rad = $c_radius ** 2 - $a ** 2;
+    my $parallel = $distance / $d;
+    my $normal   = vector(-$parallel->y, $parallel->x, 0);
+
+    my $x   = ($d**2 - $radii[1]**2 + $radii[0]**2) / (2 * $d);
+    my $rad = $radii[0]**2 - $x**2;
     return if($rad < 0);
-    my $b   = sqrt($rad);
-    
-    my @positions = ($c_center_p - $l_normal * $a + $l_parallel * $b,
-		     $c_center_p - $l_normal * $a - $l_parallel * $b);
+
+    my $y         = sqrt($rad);
+    my @positions = ($center_p[0] + $parallel * $x + $normal * $y,
+		     $center_p[0] + $parallel * $x - $normal * $y);
     my $class     = 'Math::Geometry::Construction::TemporaryPoint';
     return(map { $class->new(position => $_) } @positions);
 }
