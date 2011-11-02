01d86848026348d4f430277999d3cbceda3501b1
Lutz Gehlen <lgehlen@cpan.org>
2011-11-02 16:54:36 +1300

    same for line; need to check that $args{support} is actually an ARRAY reference

--- lib/Math/Geometry/Construction/Line.pm
+++ lib/Math/Geometry/Construction/Line.pm
@@ -54,6 +54,36 @@ has 'extend'      => (isa     => 'Num',
 		      is      => 'rw',
 		      default => 0);
 
+sub BUILDARGS {
+    my ($class, %args) = @_;
+    my $point_class    = 'Math::Geometry::Construction::Point';
+    
+    foreach(@{$args{support}}) {
+	if(!eval { $_->isa($point_class) }) {
+	    $_ = $args{construction}->add_point
+		(position => $_,
+		 hidden   => 1);
+	}
+    }
+
+    if(exists($args{radius})) {
+	$args{support} = $args{construction}->add_derived_point
+	    ('TranslatedPoint',
+	     {input      => [$args{center}],
+	      translator => [$args{radius}, 0]},
+	     {hidden     => 1});
+	delete $args{radius};
+    }
+
+    if(!eval { $args{support}->isa($point_class) }) {
+	$args{support} = $args{construction}->add_point
+	    (position => $args{support},
+	     hidden   => 1);
+    }
+
+    return \%args;
+}
+
 sub BUILD {
     my ($self, $args) = @_;
 
