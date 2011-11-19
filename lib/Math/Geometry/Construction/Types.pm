f4911b09ebb77939cf55dfe54851d259824596a5
Lutz Gehlen <lgehlen@cpan.org>
2011-11-19 19:38:50 +1300

    new Module Types; only carcass

---
+++ lib/Math/Geometry/Construction/Types.pm
@@ -0,0 +1,48 @@
+package Math::Geometry::Construction::Types;
+use Moose::Util::TypeConstraints;
+use strict;
+use warnings
+
+use 5.008008;
+
+use Math::Vector::Real;
+use Carp;
+
+=head1 NAME
+
+C<Math::Geometry::Construction::Types> - custom types for Math::Geometry::Construction
+
+=head1 VERSION
+
+Version 0.019
+
+=cut
+
+our $VERSION = '0.019';
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 AUTHOR
+
+Lutz Gehlen, C<< <perl at lutzgehlen.de> >>
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Lutz Gehlen.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
