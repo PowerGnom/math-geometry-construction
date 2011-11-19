b1dc4fb5f2fa989458fe7ebdd3c749fa69b9e4f0
Lutz Gehlen <lgehlen@cpan.org>
2011-11-19 19:20:56 +1300

    bumped version number to 0.019

--- lib/Math/Geometry/Construction.pm
+++ lib/Math/Geometry/Construction.pm
@@ -15,11 +15,11 @@ C<Math::Geometry::Construction> - intersecting lines and circles
 
 =head1 VERSION
 
-Version 0.018
+Version 0.019
 
 =cut
 
-our $VERSION = '0.018';
+our $VERSION = '0.019';
 
 
 ###########################################################################
